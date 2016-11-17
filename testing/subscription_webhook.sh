923a00e1ea5789042294bab0be7c9ae64216539d
jamcoy <james@coynemail.net>
2016-11-17 17:49:41 +0000

    Add missing trailing slash

--- testing/subscription_webhook.sh
+++ testing/subscription_webhook.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-curl -H "Content-Type: application/json" -X POST -d @subscription_webhook.json http://localhost:8000/subscriptions_webhook
+curl -H "Content-Type: application/json" -X POST --data-binary @subscription_webhook.json http://localhost:8000/subscriptions_webhook/
+
