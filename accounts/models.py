00faff493513456b841154a1c400a9666a2a1554
jamcoy <james@coynemail.net>
2016-11-17 12:24:34 +0000

    Add login / logout

--- accounts/models.py
+++ accounts/models.py
@@ -14,8 +14,10 @@ class AccountUserManager(UserManager):
             raise ValueError('The given username must be set')
 
         email = self.normalize_email(email)
-        user = self.model(username=email, email=email,
-                          is_staff=is_staff, is_active=True,
+        user = self.model(username=email,
+                          email=email,
+                          is_staff=is_staff,
+                          is_active=True,
                           is_superuser=is_superuser,
                           date_joined=now, **extra_fields)
         user.set_password(password)
