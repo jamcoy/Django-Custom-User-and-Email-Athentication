983a1bfec0d41f18f7e06a6c24ba3a78a85e5b48
jamcoy <james@coynemail.net>
2016-11-19 12:56:33 +0000

    Provide invoice after sale (Challenges A and B)

--- paypal_store/views.py
+++ paypal_store/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 
 @csrf_exempt
 def paypal_return(request):
-    args = {'post': request.POST, 'get': request.GET}
+    invoice = request.POST.getlist('invoice')
+    db_id = invoice[0][0]
+    item = get_object_or_404(Product, pk=db_id)
+    args = {'post': request.POST, 'get': request.GET, 'item': item}
     return render(request, 'paypal/paypal_return.html', args)
 
 
